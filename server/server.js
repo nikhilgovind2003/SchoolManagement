e72a23c88d82bd3f5f28ff790b984ab7c2a9c6a3
nikhilgovind2003 <govindnikhil508@gmail.com>
2025-03-19 19:17:00 +0530

    vitconfig changed

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://full-stack-school-management-system.netlify.app",   // Netlify origin
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   })
